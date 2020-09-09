a26c43e65f3fe24e39de317eec7bc400655fef46
Shadowsky66 <www.shadowsky@gmail.com>
2020-09-09 14:20:25 +0500

    DAY 2

--- js/main.js
+++ js/main.js
@@ -11,14 +11,55 @@ document.addEventListener('keyup', stopRun);
 
 function startGame() {
     start.classList.add('hide');
+
+    for (let i = 0; i < 20; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
+    for (let i = 0; i < getQuantityElements(100 * settings.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * settings.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url(./images/enemy.png) center / cover';
+        gameArea.appendChild(enemy);
+    }
+
     settings.start = true;
     gameArea.appendChild(car);
+    settings.x = car.offsetLeft;
+    settings.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    console.log('playGame');
+
+    moveRoad();
+    moveEnemy();
+
     if (settings.start) {
+        if (keys.ArrowLeft && settings.x > 0) {
+            settings.x -= settings.speed;
+        }
+        if (keys.ArrowRight && settings.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            settings.x += settings.speed;
+        }
+
+        if (keys.ArrowDown && settings.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            settings.y += settings.speed;
+        }
+
+        if (keys.ArrowUp && settings.y > 0) {
+            settings.y -= settings.speed;
+        }
+
+        car.style.left = settings.x + 'px';
+        car.style.top = settings.y + 'px';
         requestAnimationFrame(playGame);
     }
 }
@@ -33,17 +74,49 @@ const keys = {
 const settings = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 5,
+    traffic: 3
 }
+
+function getQuantityElements(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+}
+
+console.log(getQuantityElements(50));
 
 function startRun(event) {
     event.preventDefault();
-    console.log(event.key);
     keys[event.key] = true;
 }
 
 function stopRun(event) {
     event.preventDefault();
-    console.log(event.key);
     keys[event.key] = false;
+}
+
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach((line) => {
+        line.y += settings.speed;
+        line.style.top = line.y + 'px';
+
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+    })
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll('.enemy');
+    enemy.forEach((item) => {
+        item.y += settings.speed / 2;
+        item.style.top = item.y + 'px';
+
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * settings.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+        }
+    });
+
+
 }
